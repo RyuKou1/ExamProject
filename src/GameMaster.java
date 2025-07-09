a8d20791fed2628add609486486eac6b1769eb9f
241105 <241105@std.hi-joho.ac.jp>
2025-07-09 10:45:20 +0900

    ExamProject

--- src/GameMaster.java
+++ src/GameMaster.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 
 public class GameMaster {
     public static void main(String[] args) {
-        ArrayList<String> party = new ArrayList<String>();
-        ArrayList<String> monsters = new ArrayList<String>();
+        ArrayList<Character> party = new ArrayList<Character>();
+        ArrayList<Monster> monsters = new ArrayList<Monster>();
         party.add("勇者");
         party.add("魔法使い");
         party.add("盗賊");
+
     }
 }
