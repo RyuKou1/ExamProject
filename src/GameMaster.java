bd88f9119f4f9e698cddff7a17a7b03b4dc10362
241105 <241105@std.hi-joho.ac.jp>
2025-07-16 10:02:39 +0900

    ExamProject

--- src/GameMaster.java
+++ src/GameMaster.java
@@ -5,27 +5,27 @@ public class GameMaster {
         ArrayList<Character> party = new ArrayList<>();
         ArrayList<Monster> monsters = new ArrayList<>();
 
-        Hero h = new Hero("勇者",100,"剣");
-        party.add(h);
-        Wizard w = new Wizard("魔法使い",60,20);
-        party.add(w);
-        Thief t = new Thief("盗賊",70);
-        party.add(t);
-        Matango m = new Matango('A',45);
-        monsters.add(m);
-        Goblin g = new Goblin('A',50);
-        monsters.add(g);
-        Slime s = new Slime('A',40);
-        monsters.add(s);
+        Hero hero = new Hero("勇者",100,"剣");
+        party.add(hero);
+        Wizard wizard = new Wizard("魔法使い",60,20);
+        party.add(wizard);
+        Thief thief = new Thief("盗賊",70);
+        party.add(thief);
+        Matango matango = new Matango('A',45);
+        monsters.add(matango);
+        Goblin goblin = new Goblin('A',50);
+        monsters.add(goblin);
+        Slime slime = new Slime('A',40);
+        monsters.add(slime);
 
         System.out.println("---味方パーティ---");
-        h.showStatus();
-        w.showStatus();
-        t.showStatus();
+        hero.showStatus();
+        wizard.showStatus();
+        thief.showStatus();
         System.out.println("---敵グループ---");
-        m.showStatus();
-        g.showStatus();
-        s.showStatus();
+        matango.showStatus();
+        goblin.showStatus();
+        slime.showStatus();
 
         System.out.println("\n味方の総攻撃！");
         for(Character i : party){
@@ -43,7 +43,7 @@ public class GameMaster {
 
         System.out.println("\nダメージを受けた勇者が突然光りだした！");
         System.out.println("勇者はスーパーヒーローに進化した！");
-        SuperHero superHero = new SuperHero(h);
+        SuperHero superHero = new SuperHero(hero);
         party.set(0,superHero);
         for(Monster j : monsters){
             superHero.attack(j);
